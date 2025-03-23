05137f4cff4cb2e6331e35da32df2f8c1375da79
Patrick Abrena <patrickabrena@hotmail.com>
2025-03-23 15:49:50 -0500

    force build through export defineConfig and wrap index.js in DOM event listener

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
     root: '.',
     publicDir: 'public',
     base: './',
-}
+    esbuild: {
+        target: "esnext",  // Allow top-level await
+        supported: {
+            'top-level-await': true,
+        }
+    }
+});
